a9f474c7643f814106df94ab8c61410c529855a2
unknown <visp.asap@gmail.com>
2022-03-25 13:06:36 +0600

    TODO: Recycler with films

---
+++ WorldCinema/app/src/main/java/com/example/worldcinema/Fragments/new_films_fragment.java
@@ -0,0 +1,145 @@
+package com.example.worldcinema.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.FrameLayout;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import com.example.worldcinema.MainScreen;
+import com.example.worldcinema.R;
+
+import java.util.Arrays;
+
+
+public class new_films_fragment extends Fragment {
+
+    private final int[] images = {R.drawable.film_cover, R.drawable.logo, R.drawable.ic_launcher_background, R.drawable.btn_main};
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private ImageSwitcher img_switcher;
+
+    private GestureDetector detector;
+
+    private int position = 0;
+
+    public new_films_fragment() {
+
+    }
+
+    public static new_films_fragment newInstance() {
+        return new new_films_fragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_films_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        img_switcher = getView().findViewById(R.id.img_switcher);
+        buildImageSwitcher();
+    }
+
+    private void buildImageSwitcher(){
+        Log.i("SWITCHER", img_switcher.toString());
+        Log.i("IMAGES", Arrays.toString(images));
+
+        Animation anim_in = new AlphaAnimation(0, 1);
+        Animation anim_out = new AlphaAnimation(1, 0);
+        anim_in.setDuration(800);
+        anim_out.setDuration(800);
+
+        img_switcher.setInAnimation(anim_in);
+        img_switcher.setOutAnimation(anim_out);
+        img_switcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setBackgroundColor(0xFF000000);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setLayoutParams(
+                        new FrameLayout.LayoutParams(
+                                FrameLayout.LayoutParams.MATCH_PARENT,
+                                FrameLayout.LayoutParams.WRAP_CONTENT
+                        )
+                );
+                return imageView;
+            }
+        });
+
+        img_switcher.setImageResource(images[0]);
+    }
+
+//    public class GD extends GestureDetector.SimpleOnGestureListener {
+//        private static final int SWIPE_MIN_DISTANCE = 120;
+//        private static final int SWIPE_MAX_OFF_PATH = 250;
+//        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+//
+//        @Override
+//        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
+//            try {
+//                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+//                    return false;
+//                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+//                    pos_plus();
+//                    img_switcher.setImageResource(images[position]);
+//                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+//                    pos_minus();
+//                    img_switcher.setImageResource(images[position]);
+//                }
+//            } catch (Exception e) {
+//                // nothing
+//            }
+//            return false;
+//        }
+//
+//        @Override
+//        public boolean onDown(MotionEvent e) {
+//            return true;
+//        }
+//
+//        private void pos_plus(){
+//            position++;
+//            if (position > images.length - 1){
+//                position = 0;
+//            }
+//        }
+//
+//        private void pos_minus(){
+//            position--;
+//            if (position < 0){
+//                position = images.length - 1;
+//            }
+//        }
+//    }
+}
--- WorldCinema/app/src/main/java/com/example/worldcinema/MainScreen.java
+++ WorldCinema/app/src/main/java/com/example/worldcinema/MainScreen.java
@@ -4,19 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageSwitcher;
+import android.widget.RelativeLayout;
+
+import com.example.worldcinema.Fragments.new_films_fragment;
 
 public class MainScreen extends AppCompatActivity {
+
+    private ImageSwitcher img_switcher;
+
+    private GestureDetector detector;
+
+    private View.OnTouchListener gestureListener;
+
+    private int position = 0;
+    private final int[] images = {R.drawable.film_cover, R.drawable.logo, R.drawable.ic_launcher_background, R.drawable.btn_main};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         getSupportActionBar().hide();
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_new_films_fragment, null, false );
+
+        img_switcher = view.findViewById(R.id.img_switcher);
+
+        detector = new GestureDetector(this, new GD());
+        gestureListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return detector.onTouchEvent(motionEvent);
+            }
+        };
+
+        img_switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        img_switcher.setOnTouchListener(gestureListener);
 
         setMainFragment();
     }
@@ -28,4 +63,46 @@ public class MainScreen extends AppCompatActivity {
         ft.commit();
     }
 
+    public class GD extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    pos_plus();
+                    img_switcher.setImageResource(images[position]);
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    pos_minus();
+                    img_switcher.setImageResource(images[position]);
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        private void pos_plus(){
+            position++;
+            if (position > images.length - 1){
+                position = 0;
+            }
+        }
+
+        private void pos_minus(){
+            position--;
+            if (position < 0){
+                position = images.length - 1;
+            }
+        }
+    }
 }
